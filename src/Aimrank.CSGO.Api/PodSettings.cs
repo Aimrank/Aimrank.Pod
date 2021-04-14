0c24c947703885f9491d2d22901974cf8fb26816
Mariusz Baran <mariusz.baran23@gmail.com>
2021-04-14 23:38:20 +0200

    Connect to cluster on startup

---
+++ src/Aimrank.CSGO.Api/PodSettings.cs
@@ -0,0 +1,8 @@
+namespace Aimrank.CSGO.Api
+{
+    public class PodSettings
+    {
+        public string ClusterAddress { get; set; }
+        public int MaxServers { get; set; }
+    }
+}
--- src/Aimrank.CSGO.Api/Program.cs
+++ src/Aimrank.CSGO.Api/Program.cs
@@ -1,17 +1,39 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
 
 namespace Aimrank.CSGO.Api
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await ConnectToCluster();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+        private static async Task ConnectToCluster()
+        {
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.PostAsJsonAsync(
+                $"{PodSettingsProvider.Settings.ClusterAddress}/api/cluster", new
+                {
+                    IpAddress = "localhost",
+                    PodSettingsProvider.Settings.MaxServers
+                });
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Could not connect to cluster.");
+            }
+        }
     }
 }
